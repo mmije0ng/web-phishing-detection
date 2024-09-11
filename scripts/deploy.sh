b7d04c90013c71162f3a279d45bab6ac937eb235
mmije0ng <victon777@naver.com>
2024-09-11 16:32:33 +0900

    CI: 워커 수 변경 후 재실행

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/hs-phishing
-# FLASK_APP_DIR=/home/ubuntu/hs-phishing
-# ENV_PATH=$FLASK_APP_DIR/.env
 cd $REPOSITORY
 
 # Flask 앱 인스턴스 종료
@@ -35,16 +33,7 @@ else
     echo "> Gunicorn is already installed"
 fi
 
-# Flask 앱 시작
-echo "> Starting Flask app with gunicorn"
-nohup gunicorn -w 4 app:app -b 0.0.0.0:5000 > gunicorn.log 2> gunicorn_error.log &
-
-# # Gunicorn 실행 확인
-# sleep 5
-# if pgrep -f gunicorn > /dev/null
-# then
-#     echo "> Gunicorn started successfully"
-# else
-#     echo "> Gunicorn failed to start"
-#     exit 1
-# fi
+# 워커 수 계산 및 Flask 앱 시작
+NUM_WORKERS=$(($(nproc) * 2 + 1))  # CPU 코어 수에 기반하여 워커 수 설정
+echo "> Starting Flask app with gunicorn using $NUM_WORKERS workers"
+nohup gunicorn -w $NUM_WORKERS --threads 2 -t 120 app:app -b 0.0.0.0:5000 > gunicorn.log 2> gunicorn_error.log &
