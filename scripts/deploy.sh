fa5d30b357f8a4689963b472e1eee890d4478b45
mmije0ng <victon777@naver.com>
2024-09-13 10:51:04 +0900

    CI: deploy.sh 가상환경 삭제 및 재설정으로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/hs-phishing
+# FLASK_APP_DIR=/home/ubuntu/hs-phishing
+# ENV_PATH=$FLASK_APP_DIR/.env
 cd $REPOSITORY
 
 # Flask 앱 인스턴스 종료
@@ -14,17 +16,14 @@ else
     sleep 5
 fi
 
-# 가상환경 확인 및 활성화
-if [ -d "$REPOSITORY/venv" ]; then
-    echo "> Using existing virtual environment"
-    source $REPOSITORY/venv/bin/activate
-else
-    echo "> Virtual environment not found, setting up new virtual environment"
-    python3 -m venv $REPOSITORY/venv
-    source $REPOSITORY/venv/bin/activate
-fi
+# 가상환경 삭제 및 재설정
+echo "> Removing existing venv directory"
+rm -rf $REPOSITORY/venv
 
-# 의존성 설치
+echo "> Setting up new virtual environment"
+python3 -m venv $REPOSITORY/venv
+source $REPOSITORY/venv/bin/activate
+
 echo "> Installing dependencies"
 pip install -r $REPOSITORY/requirements.txt > pip_install.log 2> pip_install_error.log
 
@@ -36,10 +35,9 @@ else
     echo "> Gunicorn is already installed"
 fi
 
-# 워커 수 계산 및 Flask 앱 시작
-NUM_WORKERS=$(($(nproc) * 2 + 1))  # CPU 코어 수에 기반하여 워커 수 설정
-echo "> Starting Flask app with gunicorn using $NUM_WORKERS workers"
-nohup gunicorn -w $NUM_WORKERS --threads 2 -t 120 app:app -b 0.0.0.0:5000 > gunicorn.log 2> gunicorn_error.log &
+# Flask 앱 시작
+echo "> Starting Flask app with gunicorn"
+nohup gunicorn -w 4 app:app -b 0.0.0.0:5000 > gunicorn.log 2> gunicorn_error.log &
 
 # # Gunicorn 실행 확인
 # sleep 5
